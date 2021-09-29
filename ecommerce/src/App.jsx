bee93a888ff9566db5bf0963e50656012de97ee7
Pilar <pily.isaza@gmail.com>
2021-09-29 07:43:14 -0500

    crear modulos

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,5 +1,54 @@
+import PrivateLayout from 'layouts/PrivateLayout';
+import PublicLayout from 'layouts/PublicLayout';
+import Index from 'pages/Index';
+import Admin from 'pages/admin/Index';
+import Productos from 'pages/admin/Productos';
+import Login from 'pages/auth/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'styles/styles.css';
+import Registro from 'pages/auth/Registro';
+import AuthLayout from 'layouts/AuthLayout';
+
 function App() {
-  return <div></div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route path={['/admin', '/admin/productos']}>
+            <PrivateLayout>
+              <Switch>
+                <Route path='/admin/productos'>
+                  <Productos />
+                </Route>
+                <Route path='/admin'>
+                  <Admin />
+                </Route>
+              </Switch>
+            </PrivateLayout>
+          </Route>
+          <Route path={['/login', '/registro']}>
+            <AuthLayout>
+              <Switch>
+                <Route path='/login'>
+                  <Login />
+                </Route>
+                <Route path='/registro'>
+                  <Registro />
+                </Route>
+              </Switch>
+            </AuthLayout>
+          </Route>
+          <Route path={['/']}>
+            <PublicLayout>
+              <Route path='/'>
+                <Index />
+              </Route>
+            </PublicLayout>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
