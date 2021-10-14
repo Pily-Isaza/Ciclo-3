9348e512474ab94565be2aee966e462adae2383f
dayanaco <dayanaco2980@gmail.com>
2021-10-14 15:09:35 -0500

    tienda

---
+++ Proyecto Min Tic Ciclo 3/ventasForm.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class ventasForm extends Component {
+    constructor(props) {
+
+    }
+    render() {
+        return {
+            <main>
+            <><h1 class="alinear">Buscar ventas</h1><ul class="contenedor2" style="list-style: none;">
+                <li> <i class="fas fa-search"></i>
+
+                    <br>
+                        <br>
+                            <div class="buscar">
+                                <input class="buscar2" type="search" placeholder="Por nombre" />
+
+
+                                <br>
+                                    <br>
+
+                                        <input class="buscar2" type="number" placeholder="Por identificacion" />
+                                    </li>
+                                    <br>
+                                    </div>
+                                    <div class="alinear">
+                                        <button type="submit"> Enviar datos </button>
+                                    </div>
+                                    <br>
+
+                                        <div class="alinear">
+                                            <button type="reset"> Reiniciar </button>
+                                        </div>
+                                    </ul>
+                                </main>
+                                }
+                                }
+                                }</></></></></></>
